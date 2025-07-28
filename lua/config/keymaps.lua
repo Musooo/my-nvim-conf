de64859fa7aeed3a3871bf962fc21d6703c608b0
Musooo <nicoloblack2@gmail.com>
2025-07-28 18:22:19 +0200

    update conf

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,13 @@
+vim.keymap.set('i', 'jj', '<Esc>')
+vim.keymap.set('n', '<c-j>', '<c-w>j')
+vim.keymap.set('n', '<c-k>', '<c-w>k')
+vim.keymap.set('n', '<c-h>', '<c-w>h')
+vim.keymap.set('n', '<c-l>', '<c-w>l')
+vim.keymap.set('n', '<leader>3', ':vsplit<cr>')
+vim.keymap.set('n', '<leader>4', ':ssplit<cr>')
+vim.keymap.set('n', '<leader>qq', ':q<cr>')
+vim.keymap.set('n', '<leader>ww', ':w<cr>')
+vim.keymap.set('n', '<leader>wq', ':wq<cr>')
+vim.keymap.set('n', '<c-d>', '<cmd> lua vim.diagnostic.open_float() <CR>')
+vim.keymap.set('n', '<leader>e', ':Neotree toggle<cr>')
+
---
+++ lua/config/options.lua
@@ -0,0 +1,10 @@
+vim.cmd 'colorscheme habamax'
+vim.o.tabstop = 8
+vim.o.softtabstop = 8
+vim.o.shiftwidth = 8
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.signcolumn = "yes"
+vim.o.wrap = false
+
+vim.g.mapleader = " "
